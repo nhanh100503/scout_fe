53e963085deb413a566ae9c8c3b1f09d44824027
Nguyen Hoang Anh <nhanh100503@gmail.com>
2026-03-15 12:39:27 +0700

    Update timeout

--- src/tools/axios.tool.ts
+++ src/tools/axios.tool.ts
@@ -4,7 +4,7 @@ import { useToast } from '@/composables/useToast';
 const { toast, showToast } = useToast();
 const apiClient: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 10000,
+    timeout: 40000,
     headers: { 'Content-Type': 'application/json' },
 });
 
